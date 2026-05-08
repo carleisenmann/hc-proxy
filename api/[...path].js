fe232aee47f57d9cccb08c0b47eb70c5862a4432
carleisenmann <49457281+carleisenmann@users.noreply.github.com>
2026-05-08 14:51:50 -0500

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,6 @@
 module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "null");
+res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
